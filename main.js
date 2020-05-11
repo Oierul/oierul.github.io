43612ee9cc8c328bc586a2d0f98e7e197860d95a
Ionut <ionutdanieldobos6220@gmail.com>
2020-05-11 09:41:23 +0300

    updated feedback

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ const factors = {
         exp: '2.6425',
     },
     max: {
+        mult: '0.3354',
+        exp: '2.7976',
+    },
+    avg: {
         mult: '0.3424',
         exp: '2.7315',
-    },
+    }
 };
 
 function factorize(l, mult, exp) {
@@ -25,16 +29,28 @@ function computeFactors(length) {
     let l = parseFloat(length);
     let min = factorize(l, factors.min.mult, factors.min.exp);
     let max = factorize(l, factors.max.mult, factors.max.exp);
+    let avg = factorize(l, factors.avg.mult, factors.avg.exp);
+    // console.log('called compute factors', {l, min, max, avg});
 
-    console.log('called compute factors', {l, min, max});
-    
-    return { min, max };
+    return { min, max, avg };
 }
 
 function testos(length, weight) {
-    let { min, max } = computeFactors(length);
+    let { min, max, avg } = computeFactors(length);
 
     let w = parseFloat(weight);
 
-    return w >= parseFloat(min) && w <= parseFloat(max);
+    if (w < parseFloat(min)) {
+        return 0;
+    }
+
+    if (w <= parseFloat(avg)) {
+        return 1;
+    }
+
+    if (w <= parseFloat(max)) {
+        return 2;
+    }
+
+    return 3;
 }
---
+++ store.js
@@ -0,0 +1,55 @@
+const turtleData = {
+    measurements: {},
+};
+
+const turtleStore = {
+    add: data => {
+        const uuid = create_UUID();
+        data.uuid = uuid;
+
+        turtleData.measurements[uuid] = data;
+        turtleStore.syncToLocalStorage();
+    },
+
+    remove: uuid => {
+        delete turtleData.measurements[uuid];
+        turtleStore.syncToLocalStorage();
+    },
+
+    clear: () => {
+        turtleData.measurements = {};
+        turtleStore.syncToLocalStorage();
+    },
+
+    syncToLocalStorage: () => {
+        localStorage.setItem('measurements_data', JSON.stringify(turtleData.measurements));
+    },
+
+    syncFromLocalStorage: () => {
+        let currentData = localStorage.getItem('measurements_data');
+        if (!currentData) {
+            return;
+        }
+
+        try {
+            turtleData.measurements = JSON.parse(currentData);
+        } catch (e) {
+            console.log('[Store error]', e);
+        }
+    },
+
+    measurements: () => {
+        return turtleData.measurements;
+    }
+};
+
+function create_UUID(){
+    let dt = new Date().getTime();
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+
+    return uuid;
+}
